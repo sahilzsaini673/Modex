9fb2e2b364eed5a001ddee163d133aca79480e83
sahilzsaini673 <sahilzsaini673@gmail.com>
2025-10-27 23:27:13 +0530

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -8,8 +8,11 @@ const connectDB =  async() => {
         return;
     }
 
+    const uri = process.env.MONGODB;
+    if(!uri) return Response.json({ success: false, message: 'DB URL NOT FOUND' });
+
     try {
-        await mongoose.connect(process.env.MONGODB, {
+        await mongoose.connect(uri, {
             dbName: "Modex"
         })
         isConnected = true;
